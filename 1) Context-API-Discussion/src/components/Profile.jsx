c0f071f267ff335a75d2fd3a301b837e6a74cd2d
MohdNadeem <codewithnadeem@gmail.com>
2023-10-23 17:16:37 +0530

    completed Discussion context-API

---
+++ 1) Context-API-Discussion/src/components/Profile.jsx
@@ -0,0 +1,18 @@
+import React, { useContext } from "react";
+import UserContext from "../context/UserContext";
+
+const Profile = () => {
+  const { user } = useContext(UserContext);
+  {
+    console.log(user);
+  }
+  if (!user) return <h1>Please login</h1>;
+
+  return (
+    <div>
+      <h1>Welcome : {user.username}</h1>
+    </div>
+  );
+};
+
+export default Profile;
